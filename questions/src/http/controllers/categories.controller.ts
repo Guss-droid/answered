e0bb1c4a066df3b9170b5713ba60fc4b874ddff0
Gustavo Re <gustavo.re@inforoute.com.br>
2022-10-21 15:21:22 -0300

    Feat: categories

--- questions/src/http/controllers/categories.controller.ts
+++ questions/src/http/controllers/categories.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, UseGuards, Body } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, Get } from '@nestjs/common';
 import { CategoriesService } from 'src/services/categories.service';
 import { AuthorizationGuard } from '../auth/authorization.guard';
+import { CurrentUser, IAuthUser } from '../auth/currentUser';
 
 interface ICreateCategory {
   category: string;
@@ -12,7 +13,24 @@ export class CategoriesController {
 
   @Post('')
   @UseGuards(AuthorizationGuard)
-  async createCategory(@Body() { category }: ICreateCategory) {
-    return this.categoriesService.createCategory({ category });
+  async createCategory(
+    @Body() { category }: ICreateCategory,
+    @CurrentUser() user: IAuthUser,
+  ) {
+    if (!category) {
+      throw new Error('Is not possible to create a category empty');
+    }
+
+    if (!user.isAdmin) {
+      throw new Error('User does not have permission');
+    }
+
+    return await this.categoriesService.createCategory({ category });
+  }
+
+  @Get('')
+  @UseGuards(AuthorizationGuard)
+  async getCategories() {
+    return await this.categoriesService.allCategories();
   }
 }
